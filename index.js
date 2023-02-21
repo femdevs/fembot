79cf7ed9f3f40bfbb0ad3d36f877a379190c0871
Benjamin Marsh <bmarsh@lisd.us>
2023-02-21 18:27:51 -0500

    hehe

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ client.on('messageCreate', async (message) => {
     }
 });
 
+client.on('presenceUpdate', async (oldPresence, newPresence) => {
+    if (newPresence.userId == '530748350119673896' && newPresence.status == 'offline' || newPresence.status == 'invisible') {
+        const channel = await client.channels.fetch('1040327100181270679');
+        await channel.send('Alex has finally fallen asleep.');
+    }
+})
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
