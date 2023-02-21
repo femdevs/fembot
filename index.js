caa745bb6306c0bdaf0d2083841bff94d7ff74a8
Benjamin Marsh <bmarsh@lisd.us>
2023-02-21 18:08:19 -0500

    fixes

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-const { Client, Partials, AttachmentBuilder } = require('discord.js');
+const { Client, Partials, AttachmentBuilder, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
     intents: [
-        1, // GatewayIntents.GUILDS
-        2, // GatewayIntents.GUILD_MEMBERS
-        4, // GatewayIntents.GUILD_BANS
-        8, // GatewayIntents.GUILD_EMOJIS_AND_STICKERS
-        16, // GatewayIntents.GUILD_INTEGRATIONS
-        32, // GatewayIntents.GUILD_WEBHOOKS
-        64, // GatewayIntents.GUILD_INVITES
-        128, // GatewayIntents.GUILD_VOICE_STATES
-        256, // GatewayIntents.GUILD_PRESENCES
-        512, // GatewayIntents.GUILD_MESSAGES
-        1024, // GatewayIntents.GUILD_MESSAGE_REACTIONS
-        2048, // GatewayIntents.GUILD_MESSAGE_TYPING
-        4096, // GatewayIntents.DIRECT_MESSAGES
-        8192, // GatewayIntents.DIRECT_MESSAGE_REACTIONS
-        16384 // GatewayIntents.DIRECT_MESSAGE_TYPING
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMessageReactions
     ],
     partials: [
         Partials.Channel,
         Partials.GuildMember,
         Partials.Message,
         Partials.Reaction,
-        Partials.User
+        Partials.User,
+        Partials.GuildScheduledEvent,
+        Partials.ThreadMember
     ]
 });
 
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
     const { commandName } = interaction;
     switch (commandName) {
@@ -56,7 +50,7 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-client.on('messageCreate', async message => {
+client.on('messageCreate', async (message) => {
     if (!message.content.startsWith("!") || message.author.bot) return;
     const args = message.content.replace('!', '').trim().split(' ');
     const command = args.shift().toLowerCase();
@@ -79,7 +73,7 @@ client.on('messageCreate', async message => {
             break;
         case 'thegoods':
             const file = new AttachmentBuilder(require('fs').readFileSync('./the_goods.mp3'));
-            await message.reply({ files: [file] });
+            await message.reply({ files: [file.setName('theGoods.mp3')] });
             break;
         default:
             break;
