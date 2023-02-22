b2040c9ee11c8448318d55c085a246e777cacc57
Benjamin Marsh <bmarsh@lisd.us>
2023-02-21 23:41:44 -0500

    alstolfo

--- commands/astolfo.js
+++ commands/astolfo.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: 'alstolfo',
@@ -6,8 +6,20 @@ module.exports = {
         .setName('alstolfo')
         .setDescription('Take a guess'),
     async execute(client, interaction) {
-        const { astolfo } = require('../online_assets.json')
-        const selection = Math.floor(Math.random() * astolfo.length);
-        await interaction.reply(astolfo[selection]);
+        // get a random pic from r/astolfo using the fetch API
+        // make sure that it is actually an image
+        await fetch('https://www.reddit.com/r/astolfo/random/.json')
+            .then(response => response.json())
+            .then(data => {
+                const validLinks = data.data.children.filter(post => post.data.post_hint == 'image');
+                const randomLink = validLinks[Math.floor(Math.random() * validLinks.length)];
+                const embed = new EmbedBuilder()
+                    .setTitle(randomLink.data.title)
+                    .setURL(randomLink.data.url)
+                    .setImage(randomLink.data.url)
+                    .setTimestamp()
+
+                interaction.reply({ embeds: [embed] });
+            })
     }
 }
