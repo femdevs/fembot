e524325b32223ec718aa409737ce9bb1023132b6
Benjamin Marsh <bmarsh@lisd.us>
2023-02-22 15:45:10 -0500

    reset

---
+++ commands/reset.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    name: 'reset',
+    data: new SlashCommandBuilder()
+        .setName('reset')
+        .setDescription('Reset the channel'),
+        /**
+         * @param {import('discord.js').Client} client 
+         * @param {import('discord.js').ChatInputCommandInteraction} interaction 
+         */
+    async execute(client, interaction) {
+        const { MessageActionRow, MessageButton } = require('discord.js');
+        await interaction.reply({ content: 'Resetting...' });
+        new Promise(resolve => setTimeout(resolve, 1000)).then(() => {
+            const newChannel = interaction.channel.clone();
+            interaction.channel.delete();
+            newChannel.then(channel => {
+                channel.send({ content: 'Channel reset!' });
+            })
+        })
+    }
+}
