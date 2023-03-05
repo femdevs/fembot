21e80cb47a557f18b8e40535ab51103d1589790b
Benjamin Marsh <bmarsh@lisd.us>
2023-03-04 22:52:05 -0500

    add stats

--- commands/stats.js
+++ commands/stats.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    name: 'help',
+    name: 'stats',
     data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('Take a guess'),
+        .setName('stats')
+        .setDescription('Check how many times you have ran a command!'),
     async execute(client, interaction) {
         client.db.query(`SELECT * FROM users WHERE discordId = ${interaction.user.id}`, (err, rows) => {
             if (err) throw err;
