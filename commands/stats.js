772fd5a0f7a81e93b7048e475aee41520ea344dc
Benjamin Marsh <bmarsh@lisd.us>
2023-03-04 23:05:34 -0500

    add stats

--- commands/stats.js
+++ commands/stats.js
@@ -8,6 +8,7 @@ module.exports = {
     async execute(client, interaction) {
         client.db.query(`SELECT * FROM users WHERE discordId = ${interaction.user.id}`, (err, rows) => {
             if (err) throw err;
+            if (rows.length < 1) return interaction.reply({ content: 'You have not ran any commands yet!' });
             interaction.reply({ content: `You have ran ${rows[0].commandsRan} commands!` });
         });
     }
