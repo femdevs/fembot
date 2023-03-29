b1b593f1cc47a385361f4dc5e0f4817a2df4ae15
Benjamin M <benpai@sparty18.com>
2023-03-28 20:05:45 -0400

    try 1

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
     name: 'serverinfo',
@@ -6,18 +6,61 @@ module.exports = {
         .setName('serverinfo')
         .setDescription('Shows information about the server'),
     async execute(client, interaction) {
+        const data = {
+            guildId: interaction.guild.id,
+            guildName: interaction.guild.name,
+            guildOwner: client.users.cache.get(interaction.guild.ownerId),
+            guildMembers: {
+                total: interaction.guild.memberCount,
+                bots: interaction.guild.members.cache.filter(member => member.user.bot).size,
+                humans: interaction.guild.members.cache.filter(member => !member.user.bot).size,
+            },
+            guildChannels: {
+                total: interaction.guild.channels.cache.size,
+                text: interaction.guild.channels.cache.filter(channel => channel.type === ChannelType.GuildText).size,
+                voice: interaction.guild.channels.cache.filter(channel => channel.type === ChannelType.GuildVoice).size,
+                category: interaction.guild.channels.cache.filter(channel => channel.type === ChannelType.GuildCategory).size,
+                announcements: interaction.guild.channels.cache.filter(channel => channel.type === ChannelType.GuildAnnouncement).size,
+                stage: interaction.guild.channels.cache.filter(channel => channel.type === ChannelType.GuildStageVoice).size,
+            },
+            guildRoles: interaction.guild.roles.cache.size,
+            guildCreatedAt: interaction.guild.createdAt,
+            guildEmojis: {
+                total: interaction.guild.emojis.cache.size,
+                animated: interaction.guild.emojis.cache.filter(emoji => emoji.animated).size,
+                static: interaction.guild.emojis.cache.filter(emoji => !emoji.animated).size,
+            },
+            guildIcon: interaction.guild.iconURL({ dynamic: true }),
+        }
         const embed = new EmbedBuilder()
             .setTitle(`Info for ${interaction.guild.name}`)
-            .addFields(
-                {name: 'Owner', value: `<@${interaction.guild.ownerId}>`, inline: true},
-                {name: 'Members', value: `${interaction.guild.memberCount}`, inline: true},
-                {name: 'Channels', value: `${interaction.guild.channels.cache.size}`, inline: true},
-                {name: 'Roles', value: `${interaction.guild.roles.cache.size}`, inline: true},
-                {name: 'Created At', value: `${interaction.guild.createdAt}`, inline: true},  // TODO: format date
-                {name: 'Emojis', value: `${interaction.guild.emojis.cache.size}`, inline: true},
+            .addFields({
+                name: 'Owner',
+                value: data.guildOwner.toString(),
+            },
+                {
+                    name: 'Members',
+                    value: `Total: ${data.guildMembers.total}\nBots: ${data.guildMembers.bots}\nHumans: ${data.guildMembers.humans}`,
+                },
+                {
+                    name: 'Channels',
+                    value: `Total: ${data.guildChannels.total}\nText: ${data.guildChannels.text}\nVoice: ${data.guildChannels.voice}\nCategory: ${data.guildChannels.category}\nAnnouncements: ${data.guildChannels.announcements}\nStage: ${data.guildChannels.stage}`,
+                },
+                {
+                    name: 'Roles',
+                    value: data.guildRoles,
+                },
+                {
+                    name: 'Emojis',
+                    value: `Total: ${data.guildEmojis.total}\nAnimated: ${data.guildEmojis.animated}\nStatic: ${data.guildEmojis.static}`,
+                },
+                {
+                    name: 'Created At',
+                    value: data.guildCreatedAt,
+                },
             )
             .setColor(0xa331d4)
-            .setThumbnail(interaction.guild.iconURL({ dynamic: true }));
+            .setThumbnail(data.guildIcon);
         await interaction.reply({ embeds: [embed] });
     }
 }
