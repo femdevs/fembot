5ea774a7b1563943a651a8149066b1c0d8bcc62a
Benjamin <benpai@sparty18.com>
2023-03-30 22:20:56 +0000

    attachments

--- commands/announce.js
+++ commands/announce.js
@@ -6,40 +6,47 @@ module.exports = {
         .setName('announce')
         .setDescription('Make an announcement using the bot.')
         .addChannelOption(option =>
-            option.setName('channel')
+            option
+                .setName('channel')
                 .setDescription('The channel to send the announcement in.')
-                .setRequired(true))
-        .addStringOption(option => 
-            option.setName('content')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('content')
                 .setDescription('Text that should be in the announcement.')
-                .setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers | PermissionFlagsBits.SendMessages),
-        /* 
-        addAttachmentOption(option =>
-            option.setName('attachment')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+        .addAttachmentOption(option =>
+            option
+                .setName('attachment')
                 .setDescription('Attach media content to go with the annoncement.')
-                .setRequired(false))
-                */
-
+                .setRequired(false)
+        ),
+    /**
+     * @param {import('discord.js').Client} client
+     * @param {import('discord.js').ChatInputCommandInteraction} interaction
+     */
     async execute(client, interaction) {
         const channel = interaction.options.getChannel('channel');
         const content = interaction.options.getString('content');
-        // const image = interaction.options.getAttachment('attachment')
+        const attachment = interaction.options.getAttachment('attachment');
 
         const announcementEmbed = new EmbedBuilder()
-        .setTitle('New announcement')
-        .setDescription(content)
-        .setAuthor({
-            name: `Sent by ${interaction.user.username}`,
-            iconURL: interaction.user.displayAvatarURL()
-        })
-        .setColor(0xe31e35);
-        
+            .setTitle('New announcement')
+            .setDescription(content)
+            .setAuthor({
+                name: `Sent by ${interaction.user.username}`,
+                iconURL: interaction.user.displayAvatarURL()
+            })
+            .setColor(0xe31e35);
+
         const confirmationEmbed = new EmbedBuilder()
             .setDescription(`**Announcement sent in ${channel}!**`)
             .setColor(0x2ed95b);
 
-        await channel.send({embeds: [announcementEmbed]});
-        await interaction.reply({embeds: [confirmationEmbed], ephemeral: true});
+        await channel.send({ embeds: [announcementEmbed], files: [attachment] });
+        await interaction.reply({ embeds: [confirmationEmbed], ephemeral: true });
     }
 }
